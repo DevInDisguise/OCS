7d2594f4703ea7186c571aaa183ac829fc0cfb27
devansh-87 <gattanidevansh06@gmail.com>
2025-02-10 11:16:53 +0530

    vercel

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{html,js}"], // Specify where Tailwind should look for content
-  theme: {
-    extend: {}, // Customize your theme here
-  },
-  plugins: [], // Add plugins if needed
-};
