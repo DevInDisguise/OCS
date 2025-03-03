5cbd0bb713d11032fbeef0653107710ee8950834
devansh-87 <gattanidevansh06@gmail.com>
2025-03-03 18:51:40 +0530

    Latest

--- api/app.js
+++ api/app.js
@@ -2,11 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const { createClient } = require("@supabase/supabase-js");
 const md5 = require("md5");
-const cors = require("cors");
 const path = require("path");
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../public")));
@@ -58,14 +56,6 @@ app.get("/data", async (req, res) => {
     }
   } catch (err) {
     res.status(500).json({ error: "Server error", details: err.message });
-  }
-});
-
-app.get("*", (req, res) => {
-  if (req.path.startsWith("/api") || req.path.includes(".")) {
-    res.status(404).send("Not found");
-  } else {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
   }
 });
 
